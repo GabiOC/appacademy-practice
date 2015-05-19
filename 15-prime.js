294ab990d96b89e081f58d0017e2e2b4362f5acf
GabiOC <oconnor.gabriele@gmail.com>
2015-05-18 21:56:28 -0400

    Add prime solution

---
+++ 15-prime.js
@@ -0,0 +1,16 @@
+/* PROBLEM
+Write a method that takes in an integer (greater than one) and returns true if it is prime; otherwise return false.
+ex. is_prime?(3) == true */
+
+function isPrime(num){
+	for(i = 2; i < num; i++){
+		if(num % i == 0){ // if num is divisible with no remainder by 2<num, it's not prime
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(isPrime(3)); // true
+console.log(isPrime(9)); // false
+console.log(isPrime(2)); // true
